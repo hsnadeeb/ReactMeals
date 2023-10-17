437e2e5b4d737fff0eae3cd05a0bcd1c086555fc
hsnadeeb <hsnadeeb007@gmail.com>
2023-10-17 21:42:58 +0530

    added a summary

---
+++ src/components/Layout/Summary.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Summary.css'
+
+const Summary=()=>{
+
+
+    return(
+        <div className="summary">
+            <h2>Delicious Food, Deliverd To You</h2>
+            <p>Choose your favorite meal from our broad slection of available meals and  enjoy a delicious lunch or dinner at home.</p>
+            <br></br>
+            <p>All our meals are cooked with high quality ingredients, just in time and ofcourse by experienced chefs.</p>
+        </div>
+
+    );
+}
+
+export default Summary;
